94aec8d1b1a4a3c45b7b174cea1963676dbc73b0
kbg0128 <kbg0128@midasit.com>
2021-07-14 17:02:15 +0900

    Spring boot initial settings

--- src/main/java/com/example/springboottoy/SpringBootToyApplication.java
+++ src/main/java/com/example/springboottoy/SpringBootToyApplication.java
@@ -1,13 +1,36 @@
 package com.example.springboottoy;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 public class SpringBootToyApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(SpringBootToyApplication.class, args);
+        System.out.println("bokunn91) main: Application started.");
+
+    }
+
+    @Bean
+    public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+        return args -> {
+
+            System.out.println("bokunn91) commandLineRunner:");
+
+            String[] beanNames = ctx.getBeanDefinitionNames();
+            Arrays.sort(beanNames);
+            for (String beanName : beanNames) {
+                System.out.println(beanName);
+            }
+
+        };
     }
 
 }
