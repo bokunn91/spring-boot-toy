973ef2c5031ba6577dd2bd1d6d502839cf24af14
kbg0128 <kbg0128@midasit.com>
2021-07-14 11:04:20 +0900

    1st

---
+++ src/main/java/com/example/springboottoy/SpringBootToyApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboottoy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootToyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootToyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springboottoy/SpringBootToyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboottoy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootToyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
